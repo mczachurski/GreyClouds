8d0895f747c7df12991d7ec903225ae82893daef
Marcin Czachurski <mczachurski@icloud.com>
2018-05-13 09:00:27 +0200

    Initial Commit

---
+++ ClearSky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClearSky
+//
+//  Created by Marcin Czachurski on 13.05.2018.
+//  Copyright © 2018 Marcin Czachurski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
